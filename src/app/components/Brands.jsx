cf98a7abc29a5fb29a2026dc6507be0c2d75676e
Lalanthawi <Lalantha150@gmail.com>
2023-09-11 01:05:37 +0530

    hero section boilerplate

---
+++ src/app/components/Brands.jsx
@@ -0,0 +1,11 @@
+export const Brands = () => {
+  return (
+    <>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+        dolorem facere sed sequi itaque! Maiores facilis velit eum expedita
+        nostrum error, at harum ullam consequuntur quasi ex, quia rem iste?
+      </p>
+    </>
+  );
+};
